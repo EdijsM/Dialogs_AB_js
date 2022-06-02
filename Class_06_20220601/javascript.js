0ac81ac6f174ff4db0095b317ce49cfdeb2125cc
edijsm <cryptobomzis@gmail.com>
2022-06-02 12:40:43 +0300

    JS button testi

---
+++ Class_06_20220601/javascript.js
@@ -0,0 +1,10 @@
+function myFunction() {
+
+    var btn = document.getElementById("teti");
+
+    if (btn.value == "dad") {
+        btn.value = "Tēti paliec";
+        btn.innerHTML = "puff";
+    }
+    
+}
